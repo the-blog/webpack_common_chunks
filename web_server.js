95ac00da3624fbcc86bc4217bd31614f9973be32
Ilya N. Zykin <ilya.zykin@toptal.com>
2018-01-26 13:41:24 +0300

    Render EJS

--- web_server.js
+++ web_server.js
@@ -2,14 +2,24 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.set('view engine', 'ejs');
+
 const PORT_NUMBER = 5000
 
-function html_file(file_name) {
+function file_path_for(file_name) {
   return path.join(__dirname + '/' + file_name)
 }
 
+function render_html(response, file_name) {
+  return response.sendFile(file_path_for(file_name))
+}
+
+function render_ejs(response, file_name) {
+  return response.render(file_path_for(file_name))
+}
+
 app.get('/', (request, response) => {
-  response.sendFile(html_file('index.html'))
+  render_ejs(response, 'index.ejs')
 })
 
 app.listen(PORT_NUMBER)
