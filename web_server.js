8d6f5b0ea6e111a7654ea8e126640bfa6cbd03ea
Ilya N. Zykin <ilya.zykin@toptal.com>
2018-01-26 13:56:47 +0300

    Some improvements

--- web_server.js
+++ web_server.js
@@ -7,20 +7,20 @@ const PORT_NUMBER = 5000
 app.set('view engine', 'ejs')
 app.locals.greetings = "Hello World!"
 
-function file_path_for(file_name) {
+function viewFilePath(file_name) {
   return path.join(__dirname + '/' + file_name)
 }
 
-function render_html(response, file_name) {
-  return response.sendFile(file_path_for(file_name))
+function renderHtml(response, file_name) {
+  return response.sendFile(viewFilePath(file_name))
 }
 
-function render_ejs(response, file_name) {
-  return response.render(file_path_for(file_name))
+function renderEJS(response, file_name) {
+  return response.render(viewFilePath(file_name))
 }
 
 app.get('/', (request, response) => {
-  render_ejs(response, 'index.ejs')
+  renderEJS(response, 'index.ejs')
 })
 
 app.listen(PORT_NUMBER)
